4c7b458831a6810c7458baa46aea3a37cd609f44
Geovane-Ievenes <geovane.candido.ievenes@gmail.com>
2020-11-04 07:34:51 -0300

    Aplication now more RESTFULL (POST method inserted)

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const express = require('express'),
 	  app = express(),
 	  server = require('http').createServer(app),
 	  io = require('socket.io')(server),
-	  path = require('path'),
-	  ip = require('ip');
+	  bodyParser = require('body-parser'),
+	  router = require('./routes');
 
-//Rotas Estáticas
+//Middlewares
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Routes
+app.use(require('./routes'));
 
 //Configurando Nunjucks
 const nunjucks = require('nunjucks');
@@ -17,52 +22,12 @@ nunjucks.configure("views", {
 	noCache: true
 })
 
-//ROTAS
-
-app.get('/' , (req, res) => {
-	res.render('index.html')
-})
-
-app.get('/chat/:name', (req, res) => {
-
-	const username = req.params.name;
-
-	res.render('chat.html', {username})
-})
-
-app.get('/messages', (req, res)=>{
-	res.render('messages.html')
-})
-
 //SOCKET HANDLERS
 const loggedUsers = [];
 
 io.on('connection', (socket) => {
 	console.log('new client connected !!');
-	user = {};
-	var myNameIndex;
 	
-	socket.on('login', userInfo => {
-		user.name = userInfo.name;
-		loggedUsers.push(user.name);
-
-		socket.broadcast.emit('new user', user.name);
-		socket.emit('gotochat', user);
-	})
-
-	socket.on('chat', (user) => {
-		myNameIndex = loggedUsers.indexOf(user.name);
-
-		console.log(user.name);
-		console.log(myNameIndex);
-		socket.emit('sendLoggedUsers', loggedUsers.slice(myNameIndex, 1))
-	})
-
-	socket.on('disconnect', (reason) => {
-		if(reason == 'ping timeout'){
-
-		}
-	})	
 })
 
 //SERVER LISTENING
